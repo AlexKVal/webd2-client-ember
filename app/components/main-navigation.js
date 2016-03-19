931f0d7c178d1dded46f700ef41d93db4e8b60cd
AlexKVal <alexkval@gmail.com>
2016-03-20 00:14:48 +0200

    use closure actions

--- app/components/main-navigation.js
+++ app/components/main-navigation.js
@@ -5,26 +5,16 @@ const { Component, inject, computed } = Ember;
 export default Component.extend({
   session: inject.service(),
 
-  authenticated: computed.oneWay('session.data.authenticated'),
-
   isLoginRoute: computed('routeName', function() {
     return this.get('routeName').match('login');
   }),
 
   actions: {
-    login() {
-      this.sendAction('toLoginRoute');
-    },
-
     logout() {
       const session = this.get('session');
       if (session.get('isAuthenticated')) {
         session.invalidate();
       }
-    },
-
-    userNamePress() {
-      this.sendAction('toLoginRoute');
     },
 
     back() {
