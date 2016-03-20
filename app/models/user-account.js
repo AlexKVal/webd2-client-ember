9cc5d64ee9d273e6883fa76113dcc6e0d53d0340
AlexKVal <alexkval@gmail.com>
2016-03-20 20:34:24 +0200

    [added] super-admins-only routes

--- app/models/user-account.js
+++ app/models/user-account.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   password: DS.attr('string'),
+  rights: DS.attr(),
   belongsTo: DS.attr('user-group')
 });
--- app/routes/user-accounts.js
+++ app/routes/user-accounts.js
@@ -1,7 +1,7 @@
 import SuperAdminsOnly from 'webd2-client-ember/routes/super-admins-only';
 
 export default SuperAdminsOnly.extend({
-  model(params) {
-    return this.store.findRecord('user-account', params.id);
-  },
+  model() {
+    return this.store.findAll('user-account');
+  }
 });
