0a29c609bf218b6d05682db59930a3600b3e3835
AlexKVal <alexkval@gmail.com>
2016-03-20 02:07:05 +0200

    use flash-messages for wrong-password feedback

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,19 +1,18 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  session: Ember.inject.service(),
+const { inject: { service }, Component } = Ember;
+
+export default Component.extend({
+  session: service(),
+  flashMessages: service(),
 
   actions: {
     authenticate(id, password) {
       this.get('session').authenticate('authenticator:jwt', { id, password })
-      .catch((reason) => {
-        this.set('errorMessage', reason);
-        Ember.run.later(this, function() {
-          this.set('errorMessage', null);
-        }, 2000);
-      });
+      .catch((reason) => this.get('flashMessages').danger(reason));
 
       this.set('password', '');
+      this.$('input[type="password"]').focus();
     }
   }
 });
