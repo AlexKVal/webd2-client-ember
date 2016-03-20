5a5b78f42b1b546f6e80f2be530bed9c377ab8a9
AlexKVal <alexkval@gmail.com>
2016-03-20 18:10:02 +0200

    [added] application level - handling errors

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin);
+export default Ember.Route.extend(ApplicationRouteMixin, {
+  flashMessages: Ember.inject.service(),
+
+  actions: {
+    error(error) {
+      const flash = this.get('flashMessages');
+
+      flash.danger(error.message, {sticky: true});
+
+      if (Array.isArray(error.errors)) {
+        error.errors.forEach((error) => {
+          const message = error.title ? `[${error.title}] ` : '';
+          flash.danger(message + error.detail, {sticky: true});
+        });
+      }
+    }
+  }
+});
