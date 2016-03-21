1757714643a3210743bfa053b2fe7c4a270375d4
AlexKVal <alexkval@gmail.com>
2016-03-22 00:28:06 +0200

    reload "user-group" if "user-account" was changed

--- app/components/user-account-form.js
+++ app/components/user-account-form.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 import messageFromError from 'webd2-client-ember/utils/message-from-error';
 
+const { service } = Ember.inject;
+
 export default Ember.Component.extend({
-  flashMessages: Ember.inject.service(),
+  flashMessages: service(),
+  store: service(),
 
   actions: {
     cancel() {
@@ -12,6 +15,7 @@ export default Ember.Component.extend({
 
     save() {
       this.model.save()
+      .then(() => this.get('store').findAll('user-group')) // update login models
       .then(() => this.get('flashMessages').success('Saved'))
       .catch((error) => this.get('flashMessages').danger(messageFromError(error), {sticky: true}));
     }
