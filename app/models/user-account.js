4f4c3845ce60358a2d2286b71c3e94d508f855f5
AlexKVal <alexkval@gmail.com>
2016-03-21 15:56:42 +0200

    [fixed] show userGroup relationship

--- app/models/user-account.js
+++ app/models/user-account.js
@@ -4,5 +4,5 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   password: DS.attr('string'),
   rights: DS.attr(),
-  belongsTo: DS.attr('user-group')
+  userGroup: DS.belongsTo('userGroup')
 });
