59aeb6f138557d3fdd165e0551e403b4a936bc72
AlexKVal <alexkval@gmail.com>
2016-03-30 19:31:03 +0300

    move action to parent route

--- app/routes/user-accounts.js
+++ app/routes/user-accounts.js
@@ -1,3 +1,27 @@
+import Ember from 'ember';
 import SuperAdminsOnly from 'webd2-client-ember/routes/authz-super-admins-only';
+import messageFromError from 'webd2-client-ember/utils/message-from-error';
 
-export default SuperAdminsOnly.extend();
+export default SuperAdminsOnly.extend({
+  flashMessages: Ember.inject.service(),
+
+  _flashError(error) {
+    this.get('flashMessages').danger(messageFromError(error), {sticky: true});
+  },
+
+  actions: {
+    delete(user) {
+      user.set('hide', true);
+      user.save()
+      .then(() => this.get('flashMessages').success(`${user.get('name')} has been deleted`))
+      .catch((error) => this._flashError(error));
+    },
+
+    undelete(user) {
+      user.set('hide', false);
+      user.save()
+      .then(() => this.get('flashMessages').success(`${user.get('name')} has been restored`))
+      .catch((error) => this._flashError(error));
+    }
+  }
+});
--- app/routes/user-accounts/index.js
+++ app/routes/user-accounts/index.js
@@ -1,30 +1,7 @@
 import Ember from 'ember';
-import messageFromError from 'webd2-client-ember/utils/message-from-error';
 
 export default Ember.Route.extend({
-  flashMessages: Ember.inject.service(),
-
   model() {
     return this.store.findAll('user-account');
-  },
-
-  _flashError(error) {
-    this.get('flashMessages').danger(messageFromError(error), {sticky: true});
-  },
-
-  actions: {
-    delete(user) {
-      user.set('hide', true);
-      user.save()
-      .then(() => this.get('flashMessages').success(`${user.get('name')} has been deleted`))
-      .catch((error) => this._flashError(error));
-    },
-
-    undelete(user) {
-      user.set('hide', false);
-      user.save()
-      .then(() => this.get('flashMessages').success(`${user.get('name')} has been restored`))
-      .catch((error) => this._flashError(error));
-    }
   }
 });
