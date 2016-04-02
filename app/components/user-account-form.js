72fb3bccb391431dbdda5eabcd62e7c458e22e57
AlexKVal <alexkval@gmail.com>
2016-04-02 23:04:48 +0300

    [added] recognize model relations' changes

--- app/components/user-account-form.js
+++ app/components/user-account-form.js
@@ -8,14 +8,35 @@ export default Ember.Component.extend({
 
   rights: [1, 2, 3, 4, 5, 6], // TODO use real rights
 
+  _savePreviousModelRelations() {
+    this.set('previousUserGroup', this.get('model.userGroup'));
+  },
+  _rollbackModelRelations() {
+    this.set('model.userGroup', this.get('previousUserGroup'));
+  },
+
+  init() {
+    this._super(...arguments);
+    this._savePreviousModelRelations();
+  },
+
+  hasUnsavedChanges: Ember.computed('model.hasDirtyAttributes', 'model.userGroup', function() {
+    if (this.get('previousUserGroup.id') !== this.get('model.userGroup.id')) {
+      return true;
+    }
+    return this.get('model.hasDirtyAttributes');
+  }),
+
   actions: {
     cancel() {
       this.model.rollbackAttributes();
+      this._rollbackModelRelations();
       history.back();
     },
 
     save() {
       this.model.save()
+      .then(() => this._savePreviousModelRelations())
       .then(() => this.get('flashMessages').success('Saved'))
       .catch((error) => this.get('flashMessages').danger(messageFromError(error), {sticky: true}));
     },
--- app/models/user-account.js
+++ app/models/user-account.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   password: DS.attr('string'),
-  rights: DS.attr(),
+  rights: DS.attr('number'),
   userGroup: DS.belongsTo('userGroup'),
   hide: DS.attr('boolean', { defaultValue: false }),
 
