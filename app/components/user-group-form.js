c121508b9728fddf6def785126559748d0d57f50
AlexKVal <alexkval@gmail.com>
2016-05-11 10:25:00 +0300

    add user-group-form component

---
+++ app/components/user-group-form.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+import messageFromError from 'webd2-client-ember/utils/message-from-error';
+
+const { service } = Ember.inject;
+
+export default Ember.Component.extend({
+  flashMessages: service(),
+
+  actions: {
+    cancel() {
+      this.model.rollbackAttributes();
+      history.back();
+    },
+
+    save() {
+      this.model.save()
+      .then(() => this.get('flashMessages').success('Saved'))
+      .catch((error) => this.get('flashMessages').danger(messageFromError(error), {sticky: true}));
+    },
+
+    undelete(unit) {
+      this.get('onUndelete')(unit);
+    }
+  }
+});
