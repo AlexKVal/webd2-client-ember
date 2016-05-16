59adf2d75534ec863324f59e1de011390102a23d
AlexKVal <alexkval@gmail.com>
2016-05-16 11:48:33 +0300

    rewrite userGroups sorting and filtering

--- app/models/user-group.js
+++ app/models/user-group.js
@@ -1,10 +1,14 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 const { attr, hasMany } = DS;
+const { computed } = Ember;
 
 export default DS.Model.extend({
   name: attr('string'),
   hide: attr('boolean', { defaultValue: false }),
   info: attr('string'),
-  users: hasMany('user')
+  users: hasMany('user', { async: false }), // embedded
+
+  hasUsers: computed.gt('users.length', 0)
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -3,6 +3,12 @@ import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-
 
 export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   model() {
-    return this.store.query('user-group', { includeJoined: true });
+    return this.store.query('user-group', {
+      includeJoined: true,
+      filter: {hide: false}
+    })
+    .then((groups) => {
+      return groups.filterBy('hasUsers').sortBy('name');
+    });
   }
 });
