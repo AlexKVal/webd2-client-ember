bb0c7f36fc2685c21b73d9170f52b20013f1b004
AlexKVal <alexkval@gmail.com>
2016-05-16 11:24:42 +0300

    move authenticate() up to application route

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,30 +1,9 @@
 import Ember from 'ember';
 
-const { inject: { service }, Component } = Ember;
-
-export default Component.extend({
-  session: service(),
-  flashMessages: service(),
-
+export default Ember.Component.extend({
   actions: {
-    authenticate(id, password) {
-      this.get('session').authenticate('authenticator:jwt', { id, password })
-      .catch((response) => {
-        const flash = this.get('flashMessages');
-
-        if (!Array.isArray(response.errors)) {
-          return flash.danger(response, {sticky: true});
-        }
-
-        response.errors.forEach((error) => {
-          if (error.title === 'Unauthorized') {
-            flash.danger('Wrong password');
-          } else {
-            flash.danger(error.detail, {sticky: true});
-          }
-        });
-      });
-
+    login() {
+      this.sendAction('loginAction', this.get('user.id'), this.get('password'));
       this.set('password', '');
     }
   }
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,12 +2,34 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 import messageFromError from 'webd2-client-ember/utils/message-from-error';
 
+const { inject: { service } } = Ember;
+
 export default Ember.Route.extend(ApplicationRouteMixin, {
-  flashMessages: Ember.inject.service(),
+  flashMessages: service(),
+  session: service(),
 
   actions: {
     error(error) {
       this.get('flashMessages').danger(messageFromError(error), {sticky: true});
+    },
+
+    authenticate(id, password) {
+      this.get('session').authenticate('authenticator:jwt', { id, password })
+      .catch((response) => {
+        const flash = this.get('flashMessages');
+
+        if (!Array.isArray(response.errors)) {
+          return flash.danger(response, {sticky: true});
+        }
+
+        response.errors.forEach((error) => {
+          if (error.title === 'Unauthorized') {
+            flash.danger('Wrong password');
+          } else {
+            flash.danger(error.detail, {sticky: true});
+          }
+        });
+      });
     }
   }
 });
